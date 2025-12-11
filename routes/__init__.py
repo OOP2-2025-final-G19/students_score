4d9bd25873e445c9e7cc1306667e380e1da1dcd0
kinoko-1015 <ryohei1774@gmail.com>
2025-12-11 14:45:33 +0900

    トップページの編集

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,10 +1,10 @@
 from .user import user_bp
-from .product import product_bp
-from .order import order_bp
+from .subject import subject_bp
+from .score import score_bp
 
 # Blueprintをリストとしてまとめる
 blueprints = [
   user_bp,
-  product_bp,
-  order_bp
+  subject_bp,
+  score_bp
 ]
