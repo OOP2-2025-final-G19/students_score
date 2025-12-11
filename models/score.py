57a7ca8ec4d8a355f68946c8ba9432d798ded56e
1018hiroto <imaihiroto1018@gmail.com>
2025-12-11 15:19:11 +0900

    修正

--- models/score.py
+++ models/score.py
@@ -3,10 +3,10 @@ from .db import db
 from .user import User
 from .product import Product
 
-class Order(Model):
-    user = ForeignKeyField(User, backref='orders')
-    product = ForeignKeyField(Product, backref='orders')
-    order_date = DateTimeField()
+class Score(Model):
+    user = ForeignKeyField(User, backref='scores')
+    product = ForeignKeyField(Product, backref='scores')
+    score_date = DateTimeField()
 
     class Meta:
         database = db
